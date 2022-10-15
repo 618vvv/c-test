59630d575a247e7b405110fb50638d7cb5fa66e7
618vvv <422943969@qq.com>
2022-10-15 11:07:18 +0800

    first commit

---
+++ A2/a26.cpp
@@ -0,0 +1,21 @@
+/* 6．编写一个程序，其main( )调用一个用户定义的函数（以光年值
+为参数，并返回对应天文单位的值）。该程序按下面的格式要求用户输
+入光年值，并显示结果： */
+
+#include<iostream>
+
+using namespace std;
+
+double transform(double a)
+{
+    return 63240*a;
+}
+int main()
+{
+    float a;
+    cout<<"Enter the number of light years: ";
+    cin>>a;
+    cout<<a<<" light years = "<<transform(a)<<" astronomical units. "<<endl;
+    system("pause");
+    return 0;
+}
---
+++ A2/a27.cpp
@@ -0,0 +1,21 @@
+/* 7．编写一个程序，要求用户输入小时数和分钟数。在main( )函数
+中，将这两个值传递给一个void函数，后者以下面这样的格式显示这两
+个值 */
+
+#include<iostream>
+using namespace std;
+void time(int a,int b)
+{
+    cout<<"Time: "<<a<<" : "<<b<<endl;
+}
+int main()
+{
+    int a,b;
+    cout<<"Enter the number of hours: ";
+    cin>>a;
+    cout<<"Enter the number of minutes: ";
+    cin>>b;
+    time(a,b);
+    system("pause");
+    return 0;
+}
